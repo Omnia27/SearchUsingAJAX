fb41faa7c6bd8b07a6f985502730c7fca2853d6a
Omnia27 <omniaiti38@gmail.com>
2018-12-27 18:04:09 -0800

    First commit

--- mvc_test_course/App_Start/BundleConfig.cs
+++ mvc_test_course/App_Start/BundleConfig.cs
@@ -10,6 +10,8 @@ namespace mvc_test_course
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                      "~/Scripts/jquery-{version}.js","~/Scripts/jquery.unobtrusive-ajax.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
